0788eed8458534e309bd01ba13c60509acee01c5
JaimeDevS <jaime_mendes91@yahoo.com.br>
2025-08-27 22:41:45 -0300

    primeiro commit

---
+++ backend/src/main/java/com/globalit/contacorrente/SistemaDeAgendamentoDeTranferenciasFinanceirasApplication.java
@@ -0,0 +1,13 @@
+package com.globalit.contacorrente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeAgendamentoDeTranferenciasFinanceirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeAgendamentoDeTranferenciasFinanceirasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/globalit/contacorrente/SistemaDeAgendamentoDeTranferenciasFinanceirasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalit.contacorrente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeAgendamentoDeTranferenciasFinanceirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
